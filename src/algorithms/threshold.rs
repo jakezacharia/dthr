66fa367970bd2c218c7eb7914c66aab6796593ab
Jacob Zacharia <jacobzach@pm.me>
2025-11-13 21:19:00 -0800

    updated threshold into its own file

--- src/algorithms/threshold.rs
+++ src/algorithms/threshold.rs
@@ -3,7 +3,6 @@ pub fn threshold(buffer: &mut [u8], height: u32, width: u32, threshold: u8) {
     for y in 0..height {
         for x in 0..width {
             let i = (y * width + x) as usize;
-
             buffer[i] = if buffer[i] > threshold { 255 } else { 0 };
         }
     }
